508468acf8261e092c640a10fe1c85b1bbfa31af
pisce <pis.ceyatta@gmail.com>
2020-12-08 23:42:08 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+def main():
+    N = int(input("問題数 >"))
+    cycle = int(input("見開きの問題数 >"))
+    time_out = int(input("制限時間 [s] (必要ない場合は -1)"))
+    time_out = time_out if time_out != -1 else 10000
+
+
+if __name__ == "__main__":
+    print("Hello, World")
